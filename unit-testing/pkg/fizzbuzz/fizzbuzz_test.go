d4978e12db17948eb0770be0c72a5a9d1641a04e
Clay Raynor <cd.raynor@gmail.com>
2022-06-22 15:42:35 -0400

    Update test structure

--- unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
+++ unit-testing/pkg/fizzbuzz/fizzbuzz_test.go
@@ -5,12 +5,11 @@ import (
 	"testing"
 )
 
+const fizz = "Fizz"
+const buzz = "Buzz"
+const fizzbuzz = "FizzBuzz"
+
 func TestFizzBuzzValidEntries(t *testing.T) {
-
-	const fizz = "Fizz"
-	const buzz = "Buzz"
-	const fizzbuzz = "FizzBuzz"
-
 	result := []string{"1", fizz, "3", fizz, buzz, fizz, "7", fizz, "9", fizzbuzz}
 
 	assert.Equal(t, FizzBuzz(10, 2, 5), result)
