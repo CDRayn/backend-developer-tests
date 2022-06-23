c0bc5914abb4bae20fa4845e3d2ebd5e93c02925
Clay Raynor <cd.raynor@gmail.com>
2022-06-22 23:01:43 -0400

    Add routers

--- rest-service/main.go
+++ rest-service/main.go
@@ -1,12 +1,48 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/stackpath/backend-developer-tests/rest-service/pkg/models"
 )
 
 func main() {
 	fmt.Println("SP// Backend Developer Test - RESTful Service")
 	fmt.Println()
 
-	// TODO: Add RESTful web service here
+	peopleHandler := func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			w.Header().Set("Content-Type", "application/json")
+			_, err := w.Write([]byte("Method not allowed!\r\n"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			return
+		}
+
+		if len(req.URL.Query()) == 0 {
+			msg, err := json.Marshal(models.AllPeople())
+			if err != nil {
+				log.Fatal(err)
+			}
+			_, err = w.Write(append(msg, []byte("\r\n")...))
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+	}
+	//peopleIdHandler := func(w http.ResponseWriter, req *http.Request) {
+	//
+	//}
+
+	http.HandleFunc("/people", peopleHandler)
+	//http.HandleFunc("/people/:", peopleIdHandler)
+	// Bind to a TCP port and listen for incoming networking traffic
+	// Use the non-privileged TCP port 8000
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
